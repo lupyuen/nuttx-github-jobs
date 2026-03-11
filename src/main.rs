5a944a3f8e5273789d2c2d8ba3e036e1e36c1ffa
Lup Yuen Lee <luppy@appkaki.com>
2026-03-11 21:03:29 +0800

    Read Job JSON
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ fn main() {
     let file = File::open(format!("job/{}.json", JOB_ID)).unwrap();
     let reader = BufReader::new(file);
     let job: serde_json::Value = serde_json::from_reader(reader).unwrap();
-    println!("\njob=\n{}", serde_json::to_string_pretty(&job).unwrap());
+    // println!("\njob=\n{}", serde_json::to_string_pretty(&job).unwrap());
 
     // Read a Job Duration
     let duration = read_to_string(format!("duration/{}.txt", JOB_ID)).unwrap();
-    println!("\njob_duration={}", duration);
+    // println!("\njob_duration={}", duration);
 
     // Update the Job JSON with the duration
     let mut job_with_duration = job.clone();
     job_with_duration["run_duration_ms"] = serde_json::Value::String(duration.trim().to_string());
-    println!("\njob_with_duration=\n{}", serde_json::to_string_pretty(&job_with_duration).unwrap());
+    // println!("\njob_with_duration=\n{}", serde_json::to_string_pretty(&job_with_duration).unwrap());
 
     // Dump the Job Fields
     for field in JOB_FIELDS.iter() {
@@ -29,13 +29,13 @@ fn main() {
     let file = File::open(format!("pr/{}.json", PR_NUMBER)).unwrap();
     let reader = BufReader::new(file);
     let pr: serde_json::Value = serde_json::from_reader(reader).unwrap();
-    println!("\npr=\n{}", serde_json::to_string_pretty(&pr).unwrap());
+    // println!("\npr=\n{}", serde_json::to_string_pretty(&pr).unwrap());
 
     // Flatten the labels into "Arch: arm, Size: M"
     let mut labels_str = String::new();
     if let Some(labels) = pr["labels"].as_array() {
-        for (i, label) in labels.iter().enumerate() {
-            println!("pr_label_{}={}", i, label["name"]);
+        for (_i, label) in labels.iter().enumerate() {
+            // println!("pr_label_{}={}", i, label["name"]);
             if let Some(name) = label["name"].as_str() {
                 if !labels_str.is_empty() {
                     labels_str.push_str(", ");
@@ -60,7 +60,7 @@ fn main() {
     pr_with_labels["headRepositoryOwner"] = serde_json::Value::String(headRepositoryOwner.to_string());
     pr_with_labels["mergeCommit"] = serde_json::Value::String(mergeCommit.to_string());
     pr_with_labels["mergedBy"] = serde_json::Value::String(mergedBy.to_string());
-    println!("\npr_with_labels=\n{}", serde_json::to_string_pretty(&pr_with_labels).unwrap());
+    // println!("\npr_with_labels=\n{}", serde_json::to_string_pretty(&pr_with_labels).unwrap());
 
     // Dump the PR Fields
     for field in PR_FIELDS.iter() {
