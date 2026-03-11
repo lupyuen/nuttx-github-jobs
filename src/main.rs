4f677b865a0564b6285ec3900c8ca51f4079e4b4
Lup Yuen Lee <luppy@appkaki.com>
2026-03-11 20:19:57 +0800

    Read Job JSON
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::BufReader;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("job/22874413924.json").unwrap();
+    let reader = BufReader::new(file);
+    let job: serde_json::Value = serde_json::from_reader(reader).unwrap();
+    println!("\njob=\n{}", serde_json::to_string_pretty(&job).unwrap());
 }
+
+// PR: id,url,updatedAt,title,additions,assignees,author,autoMergeRequest,baseRefName,changedFiles,closed,closedAt,createdAt,deletions,files,headRefName,headRefOid,headRepository,headRepositoryOwner,isDraft,labels,mergeCommit,mergeStateStatus,mergeable,mergedAt,mergedBy,milestone,number,state
+// Job: conclusion,createdAt,databaseId,displayTitle,event,headBranch,headSha,name,number,startedAt,status,updatedAt,url,workflowDatabaseId,workflowName
