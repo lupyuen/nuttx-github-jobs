39dbdf6d4123aa3916495feacab2772cd92b1a8d
Lup Yuen Lee <luppy@appkaki.com>
2026-03-11 20:42:33 +0800

    Read Job JSON
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,43 @@ use std::fs::{File, read_to_string};
 
 fn main() {
     // Read a Job JSON
-    let file = File::open("job/22874413924.json").unwrap();
+    const JOB_ID: u64 = 20304392258;
+    let file = File::open(format!("job/{}.json", JOB_ID)).unwrap();
     let reader = BufReader::new(file);
     let job: serde_json::Value = serde_json::from_reader(reader).unwrap();
     println!("\njob=\n{}", serde_json::to_string_pretty(&job).unwrap());
 
+    // Read a Job Duration
+    let duration = read_to_string(format!("duration/{}.txt", JOB_ID)).unwrap();
+    println!("\njob_duration={}", duration);
+
+    // Update the Job JSON with the duration
+    let mut job_with_duration = job.clone();
+    job_with_duration["run_duration_ms"] = serde_json::Value::String(duration.trim().to_string());
+    println!("\njob_with_duration=\n{}", serde_json::to_string_pretty(&job_with_duration).unwrap());
+
+    // Dump the Job Fields
     for field in JOB_FIELDS.iter() {
-        println!("job_{}={}", field, job[field]);
+        println!("job_{}={}", field, job_with_duration[field]);
     }
 
     // Read a PR JSON
-    let file = File::open("pr/18511.json").unwrap();
+    const PR_NUMBER: u32 = 17538;
+    let file = File::open(format!("pr/{}.json", PR_NUMBER)).unwrap();
     let reader = BufReader::new(file);
     let pr: serde_json::Value = serde_json::from_reader(reader).unwrap();
     println!("\npr=\n{}", serde_json::to_string_pretty(&pr).unwrap());
 
+    // Flatten the labels
+    if let Some(labels) = pr["labels"].as_array() {
+        for (i, label) in labels.iter().enumerate() {
+            println!("pr_label_{}={}", i, label);
+        }
+    }
+
     for field in PR_FIELDS.iter() {
         println!("pr_{}={}", field, pr[field]);
     }
-
-    // Read a Job Duration
-    let duration = read_to_string("duration/20994614931.txt").unwrap();
-    println!("\njob_duration={}", duration);
 }
 
 // PR Fields: id,url,updatedAt,title,additions,assignees,author,autoMergeRequest,baseRefName,changedFiles,closed,closedAt,createdAt,deletions,files,headRefName,headRefOid,headRepository,headRepositoryOwner,isDraft,labels,mergeCommit,mergeStateStatus,mergeable,mergedAt,mergedBy,milestone,number,state
@@ -61,7 +76,7 @@ const PR_FIELDS: [&'static str; 29] = [
 ];
 
 // Job Fields: conclusion,createdAt,databaseId,displayTitle,event,headBranch,headSha,name,number,startedAt,status,updatedAt,url,workflowDatabaseId,workflowName
-const JOB_FIELDS: [&'static str; 15] = [
+const JOB_FIELDS: [&'static str; 16] = [
     "conclusion", 
     "createdAt", 
     "databaseId", 
@@ -71,6 +86,7 @@ const JOB_FIELDS: [&'static str; 15] = [
     "headSha", 
     "name", 
     "number", 
+    "run_duration_ms",
     "startedAt", 
     "status", 
     "updatedAt", 
